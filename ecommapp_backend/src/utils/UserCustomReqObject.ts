dce4ec54ba63240e8150f5b18807a60d32a6e17e
Ronit Panda <pandaronit25@gmail.com>
2021-11-27 09:44:36 +0530

    made some folder changes

--- ecommapp_backend/src/utils/UserCustomReqObject.ts
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-import { Request } from 'express';
-import { UserDocument } from '../models/user.model';
-
-export interface CustomReq extends Request {
-  user: UserDocument; // or any other type
-}
